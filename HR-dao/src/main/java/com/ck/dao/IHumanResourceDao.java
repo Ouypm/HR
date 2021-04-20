946e352b4ec92dce945e0fb0bcfd03e310627eb5
songqb <1941063873@qq.com>
2021-04-20 10:53:26 +0800

    宋第二次提交

--- HR-dao/src/main/java/com/ck/dao/IHumanResourceDao.java
+++ HR-dao/src/main/java/com/ck/dao/IHumanResourceDao.java
@@ -28,7 +28,26 @@ public interface IHumanResourceDao {
     public T_HumanFile_dig queryById_HumanFile_dig(int hid);
 
     //复核修改人力资源
-    @Update("update t_HumanFile_dig set ")
+
+    //复核修改人力资源
+    @Update("update t_HumanFile_dig set human_pro_designation = " +
+            "#{human_pro_designation},human_name=#{human_name}," +
+            "human_sex=#{human_sex},human_postcode=#{human_postcode}," +
+            "human_telephone=#{human_telephone},human_qq=#{human_qq}," +
+            "human_mobilephone=#{human_mobilephone},human_nationality=#{human_nationality}," +
+            "human_address=#{human_address},human_email=#{human_email}," +
+            "human_birthplace=#{human_birthplace},human_race=#{human_race}," +
+            "human_religion=#{human_religion},human_society_security_id=#{human_society_security_id}," +
+            "human_party=#{human_party},human_id_card=#{human_id_card}," +
+            "human_age=#{human_age},human_educated_degree=#{human_educated_degree}," +
+            "human_educated_years=#{human_educated_years},human_educated_major=#{human_educated_major}," +
+            "salary_standard_name=#{salary_standard_name},human_name=#{human_name}," +
+            "human_bank=#{human_bank},human_account=#{human_account}," +
+            "human_speciality=#{human_speciality},human_hobby=#{human_hobby}," +
+            "human_family_membership=#{human_family_membership},human_histroy_records=#{human_histroy_records}," +
+            "human_picture=#{human_picture},attachment_name=#{attachment_name}," +
+            "remark=#{remark},check_time=#{check_time}" +
+            " where hfd_id = #{hfd_id} ")
     public void updateHumanResource(T_HumanFile_dig THumanFile_dig);
 
     /* 多重查询   调动 */
